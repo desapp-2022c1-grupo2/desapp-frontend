be8abdeaf00436f8850c461a1f21cd45f4c354f2
ttoloza <tomasezequieltoloza@gmail.com>
2022-06-10 12:08:08 -0300

    Add custom icon button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,13 +3,9 @@ import {CustomButtonProps} from "./props";
 import styled from "styled-components";
 import {Button} from "@mui/material";
 
-const StyledButton = styled(Button)`
-    color:white;    
-`
 
-export const CustomButton = (props: CustomButtonProps) => {
-    const {startIcon, endIcon, value, variant, ...rest} = props;
-    return (<StyledButton variant={variant} startIcon={startIcon} endIcon={endIcon} {...rest}>
+export const CustomButton = ({startIcon, endIcon, color, ...props}: CustomButtonProps) => {
+    return (<Button variant={'outlined'} startIcon={startIcon} endIcon={endIcon} color={color} >
         {props.children}
-    </StyledButton>);
+    </Button>);
 }
--- src/components/Button/props.ts
+++ src/components/Button/props.ts
@@ -2,12 +2,11 @@ import {IconProps} from "@mui/material/Icon/Icon";
 import {ButtonProps, Icon} from "@mui/material";
 import React from "react";
 import {OverridableStringUnion} from "@mui/types";
-import {ButtonPropsVariantOverrides} from "@mui/material/Button/Button";
+import {ButtonPropsColorOverrides, ButtonPropsVariantOverrides} from "@mui/material/Button/Button";
 
 export interface CustomButtonProps extends ButtonProps {
     startIcon?: React.ReactNode,
     endIcon?: React.ReactNode,
     value?: string,
-    variant?: OverridableStringUnion<'text' | 'outlined' | 'contained',
-        ButtonPropsVariantOverrides>;
+    variant?: OverridableStringUnion<'text' | 'outlined' | 'contained', ButtonPropsVariantOverrides>;
 }
