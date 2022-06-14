6532609a3df50323cd88653eda0f3082bbdbf862
adrian <agyaniri@gmail.com>
2022-06-14 20:35:51 -0300

    menu desplegado

--- src/components/AppBar/MuiNavBar.tsx
+++ src/components/AppBar/MuiNavBar.tsx
@@ -1,34 +1,66 @@
-import React from "react"
-import {AppBar, Toolbar, IconButton, Typography, Stack, Button} from "@mui/material";
+import React, {useState} from "react"
+import {AppBar, Toolbar, IconButton, Typography, Stack, Button, Menu, MenuItem} from "@mui/material";
 import {AvatarMenu} from "./AvatarMenu";
-import {MuiMenu} from "./MuiMenu";
+
 
 export const MuiNavBar = () => {
-    return(
+    
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+    
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
+    
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+    
+    return (
         <AppBar position='static'>
-            <Toolbar >
+            <Toolbar>
                 <IconButton
                     size='large'
                     color='inherit'
                     aria-label='logo'
                 >
-                    <AvatarMenu />
+                    <AvatarMenu/>
                 </IconButton>
                 <Typography
                     color='white'
                     variant='h6'
                     component='div'
-                    sx={{ flexGrow: 1 }}
+                    sx={{flexGrow: 1}}
                 >
                     DISEÑO INDUSTRIAL
                 </Typography>
-                <Stack direction='row' spacing={ 2 } color='white'>
+                <Stack direction='row' spacing={3} color='white'>
                     <Button color='inherit'>Usuarios</Button>
                     <Button color='inherit'>Trabajos Practicos</Button>
                     <Button color='inherit'>Mi Cuenta</Button>
-                
+                    <Button
+                        color='inherit'
+                        id='resources-button'
+                        onClick={handleClick}
+                        aria-controls={open ? 'resources-menu' : undefined}
+                        aria-haspopup='true'
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        Menu
+                    </Button>
                 </Stack>
-                <MuiMenu></MuiMenu>
+                <Menu
+                    id='resources-menu'
+                    open={open}
+                    anchorEl={anchorEl}
+                    MenuListProps={{
+                        'aria-labelledby': 'resources-button',
+                    }}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleClose}> Items 1</MenuItem>
+                    <MenuItem onClick={handleClose}> Items 2</MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
     )
