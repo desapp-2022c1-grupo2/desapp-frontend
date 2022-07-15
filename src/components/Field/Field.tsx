8572588271ac5af112ca0f5cf32b33a865a71084
Lucas Nahuel Sandoval <lukaz945@gmail.com>
2022-07-14 21:23:37 -0300

    (refactor) componente Field

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,25 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Input } from '../Input'
 import { FieldProps } from './props'
-import TextField, {TextFieldProps} from "@mui/material/TextField/TextField";
+import {
+  FieldContainer,
+  FieldLabel,
+} from './styled' 
 
-const FieldLabel = styled.label`
-  display: block;
-  text-align: left;
-  width: 100%;
-`
-
-export const FieldContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 8px;
-`
-
-export const Field = ({ label, ...props}: TextFieldProps) => {
+export const Field = ({ label, ...props}: FieldProps) => {
   return (
     <FieldContainer className={props.className}>
-      <TextField label={label} {...props}></TextField>
+      <FieldLabel htmlFor={props.id || ''}>{label}</FieldLabel>
+      <Input className='' {...props}/>
     </FieldContainer>
   )
 }
