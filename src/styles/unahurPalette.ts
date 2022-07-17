ea8e579a1c7c052a4bc8a43af4cd5cd39b554497
Lucas Nahuel Sandoval <lukaz945@gmail.com>
2022-07-17 16:49:50 -0300

    (feature) agregada paleta unahur

---
+++ src/styles/unahurPalette.ts
@@ -0,0 +1,14 @@
+export const unahurPalette = {
+  unahurBlack: { main: '#575756', contrastText: '#FFFFFF' },
+  unahurBlue: { main: '#0089CD', contrastText: '#FFFFFF' },
+  unahurCyan: { main: '#06A3C9', contrastText: '#FFFFFF' },
+  unahurGreen: { main: '#56A42C', contrastText: '#FFFFFF' },
+  unahurGreenAlt: { main: '#575756', contrastText: '#FFFFFF' },
+  unahurGrey: { main: '#CECECE', contrastText: '#FFFFFF' },
+  unahurOrange: { main: '#F08100', contrastText: '#FFFFFF' },
+  unahurRed: { main: '#E74924', contrastText: '#FFFFFF' },
+  unahurRedAlt: { main: '#EA4F51', contrastText: '#FFFFFF' },
+  unahurSoftGrey: { main: '#F1F1F1', contrastText: '#FFFFFF' },
+  unahurSoftWhite: { main: '#FAFAFA', contrastText: '#575756' },
+  unahurWhite: { main: '#FFFFFF', contrastText: '#575756' },
+}
