a6800ebaed39fdc19a8f8c472adfeac96cc0a069
Tomas Toloza <tomasezequieltoloza@gmail.com>
2022-09-06 15:03:36 -0300

    Agregada tabla de estudiantes, junto a modelos y adaptadores

---
+++ src/models/IStudent.ts
@@ -0,0 +1,19 @@
+export interface IStudent {
+  id?: number,
+  name?: string,
+  lastName?: string,
+  email?: string,
+  password?: string,
+  phone?: string,
+  dni?: string,
+  birthDate?: string,
+  materia_cursada?: number,
+  fecha_cambio_materia_cursada?: string,
+  materia_padre_cursada?: number,
+  comision?: number,
+  rondina?: number,
+  about?: string,
+  picture?: string,
+  materia2?: string,
+  habilitado?: number
+}
