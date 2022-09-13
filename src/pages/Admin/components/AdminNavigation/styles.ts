b0f3ce714968931b6e16e80bcacce7c54801ec5e
Lucas Nahuel Sandoval <lukaz945@gmail.com>
2022-09-12 21:28:58 -0300

    (fix) resaltado en navegacion

--- src/pages/Admin/components/AdminNavigation/styles.ts
+++ src/pages/Admin/components/AdminNavigation/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
-import { Tab as MuiTab, Tabs } from '@mui/material'
+import { NavLink } from 'react-router-dom'
 
-export const TabsContainer = styled(Tabs)`
+export const TabsContainer = styled.div`
   display: flex;
   align-items: center;
   height: 80px;
@@ -9,6 +9,22 @@ export const TabsContainer = styled(Tabs)`
   width: 100%;
 `
 
-export const Tab = styled(MuiTab)`
+export const Tab = styled(NavLink)`
+  align-items: center;
+  box-sizing: border-box;
+  color: #575756;
+  display: flex;
+  font-size: 0.875rem;
+  font-weight: 500;
   height: 79px;
+  justify-content: center;
+  line-height: 1.25;
+  max-width: 360px;
+  min-width: 90px;
+  overflow: hidden;
+  padding: 12px 16px;
+  text-align: center;
+  text-transform: uppercase;
+  user-select: none;
+  vertical-align: middle;
 `
