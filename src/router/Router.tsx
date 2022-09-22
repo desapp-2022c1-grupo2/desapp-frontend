d13bb23806941d2d272ea047be254b6550f1bb35
Lucas Nahuel Sandoval <lukaz945@gmail.com>
2022-09-21 22:42:29 -0300

    (fix) router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,62 +1,36 @@
+import { useAuth } from '@src/hooks/useAuth'
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import {
   BrowserRouter,
   Navigate,
   Routes,
   Route,
 } from 'react-router-dom'
-import { IAuth } from '@models'
-import { requestLogin } from '@store/auth'
-import { RootState } from '@store' 
 import { routes } from './routes'
 
-import { getAssignments } from '@store/Assignments'
-import { getCourses } from '@store/courses'
-import { getJtps, getStudents } from '@store/users'
+const LoginRoutes = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route {...routes.login}/>
+      <Route path='/*' element={<Navigate to='/login' />} />
+    </Routes>
+  </BrowserRouter>
+)
 
-
-export const Router = () => {
-  const { isLogged } = useSelector<RootState, IAuth>((state) => state.auth)
-  const dispatch = useDispatch()
-
-  React.useEffect(
-    () => {
-      if(!isLogged) {
-        const localEmail = localStorage.getItem("email")
-        const localPass = localStorage.getItem("password")
-        if (localEmail && localPass) dispatch(requestLogin({ email: localEmail, password: localPass }))
-      } else {
-        dispatch(getAssignments())
-        dispatch(getCourses())
-        dispatch(getJtps())
-        dispatch(getStudents())
-      }
-    },
-    [isLogged]
-  )
-
-  const AdminRoutes = (
+const AppRoutes = () => (
+  <BrowserRouter>
     <Routes>
       <Route {...routes.admin.home} />
       <Route {...routes.admin.account} />
       <Route {...routes.admin.assignments} />
       <Route {...routes.admin.students} />
       <Route {...routes.admin.jtps} />
-      <Route path='*' element={<Navigate to='/admin' />} />
+      <Route path='/login' element={<Navigate to='/admin/jtps' />} />
     </Routes>
-  )
-
-  const LogoutRoutes = (
-    <Routes>
-      <Route {...routes.login}/>
-      <Route path='*' element={<Navigate to='/login' />} />
-    </Routes>
-  )
-
-  return (
-  <BrowserRouter>
-    { isLogged ? AdminRoutes : LogoutRoutes }
   </BrowserRouter>
-  )
+)
+
+export const Router = () => {
+  const isLogged = useAuth()
+  return isLogged ? <AppRoutes /> : <LoginRoutes />
 }
