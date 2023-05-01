682d13a8d54723fa0dd97e3b101cb94d4fa6eaf3
Tomas Toloza <tomasezequieltoloza@gmail.com>
2023-05-01 00:31:55 -0300

    Add API_URL, fix course handling

--- src/services/authenticate.ts
+++ src/services/authenticate.ts
@@ -1,9 +1,10 @@
 import axios from "@util/axios"
 import { AuthAdapter, IAuth, IAuthResponse, ICredentials } from "@models"
+import { API_URL } from "@src/const"
 
 export const authenticate = async (credentials: ICredentials): Promise<IAuth> => {
   try {
-    const response = await axios.get('/auth/login', {
+    const response = await axios.get(API_URL + '/auth/login', {
       params: {
         username: credentials.email,
         password: credentials.password,
@@ -19,12 +20,13 @@ export const authenticate = async (credentials: ICredentials): Promise<IAuth> =>
 
 export const verifyCredentials = async(credentials: ICredentials): Promise<boolean> => {
   try {
-    const response = await axios.get('/auth/login', {
+    const response = await axios.get(API_URL + '/auth/login', {
       params: {
         username: credentials.email,
         password: credentials.password
       }
     })
+    console.log(response)
     return response.status === 200
   } catch (err) {
     console.error(err)
